96138bdec551d25f7ae0d7c27deda23a5fd4a865
Tate Mouser <114375692+tatemouser@users.noreply.github.com>
2023-12-29 18:07:40 -0500

    Initial Commit

---
+++ GymApplication/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GymApplication
+//
+//  Created by Tate Mouser on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymApplication/GymApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymApplicationApp.swift
+//  GymApplication
+//
+//  Created by Tate Mouser on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GymApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
